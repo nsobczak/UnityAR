e337210e66ebc2a5b0d9637a880298a100c60dcd
nsobczak <nicolas.sobczak@orange.fr>
2020-02-26 19:31:51 +0100

    Can now add object to irl targets

--- UnityAR/Assets/Menu/Scripts/AddObject.cs
+++ UnityAR/Assets/Menu/Scripts/AddObject.cs
@@ -9,9 +9,14 @@ public class AddObject : MonoBehaviour
     #region Parameters
     [SerializeField] private String tagClickable = "ClickableObject";
     [SerializeField] private Text objToSpawnName;
+    [SerializeField] private String tagImageTarget = "ImageTarget";
+    [SerializeField] private Text imageTargetName;
     [SerializeField] private GameObject[] prefabs;
 
     private int _objToSpawnIdx;
+
+    private GameObject[] _images;
+    private int _imageIdx;
     #endregion
 
 
@@ -21,6 +26,10 @@ public class AddObject : MonoBehaviour
     {
         _objToSpawnIdx = 0;
         UpdateNameObjToSpawn();
+
+        _images = GameObject.FindGameObjectsWithTag(tagImageTarget);
+        _imageIdx = 0;
+        UpdateNameImageTarget();
     }
 
     //____________________________________________________________________
@@ -43,15 +52,39 @@ public class AddObject : MonoBehaviour
         UpdateNameObjToSpawn();
     }
 
+    public void UpdateNameImageTarget()
+    {
+        imageTargetName.text = _images[_imageIdx].name;
+    }
+
+    public void SwitchImageIdx(bool increment = true)
+    {
+        _imageIdx += (increment ? 1 : -1);
+        if (_imageIdx <= 0)
+            _imageIdx = _images.Length - 1;
+        else if (_imageIdx >= _images.Length)
+            _imageIdx = 0;
+
+        UpdateNameImageTarget();
+    }
+
     public void AddObjectButtonClicked()
     {
         GameObject obj = prefabs[_objToSpawnIdx];
-        Debug.Log("obj: " + obj);
+        Debug.Log("spawned obj: " + obj);
 
         if (null != obj)
         {
             Transform trans = obj.GetComponent<Transform>();
-            GameObject currentObj = Instantiate(obj, trans.position, trans.rotation) ;
+            GameObject currentObj = Instantiate(obj, Vector3.zero, trans.rotation);
+            currentObj.transform.SetParent(_images[_imageIdx].transform);
+            currentObj.transform.localPosition = _images[_imageIdx].transform.position + trans.position;
+            currentObj.transform.localScale = trans.localScale;
+            Debug.Log("pos = " + currentObj.transform.position.ToString() +
+                " | rotation = " + currentObj.transform.rotation.ToString() +
+               " | localscale = " + currentObj.transform.localScale.ToString() +
+                " | lossyscale = " + currentObj.transform.lossyScale.ToString());
+
             currentObj.tag = tagClickable;
             //ObjControl.objList.Add(obj);
         }
--- UnityAR/Assets/Scripts/Utils/RotateOverTime.cs
+++ UnityAR/Assets/Scripts/Utils/RotateOverTime.cs
@@ -7,8 +7,8 @@ public class RotateOverTime : MonoBehaviour
 {
     #region Parameters
     //object to rotate
-    public float WindForce = 100.0f;
-    [SerializeField] private Vector3 RefAxe = new Vector3(0, 1, 0);
+    public float rate = 100.0f;
+    [SerializeField] private Vector3 refAxe = new Vector3(0, 1, 0);
     #endregion
 
     //____________________________________________________________________
@@ -22,6 +22,6 @@ public class RotateOverTime : MonoBehaviour
     {
         //TODO: access wind force via wind controller
         //1tr/min = 6deg/sec
-        transform.Rotate(RefAxe * WindForce * Time.deltaTime);
+        transform.Rotate(refAxe * rate * Time.deltaTime);
     }
 }
